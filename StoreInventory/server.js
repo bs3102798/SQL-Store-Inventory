d6e6072c00b7aaa550f9fb334731b517646d5a93
Burness <bsiii102798@gmail.com>
2025-02-08 22:47:43 -0800

    connect/error

--- StoreInventory/server.js
+++ StoreInventory/server.js
@@ -1,7 +1,39 @@
 const inquirer = reqiure("inquirer");
 const mysql = require("mysq12");
-const fonts = reqire('cfonts');
+const cfonts = reqire('cfonts');
 
 const connection = mysql.createConnection({
-    
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'storeInventoryTracker_db'
+});
+
+connection = connect((err) => {
+    if (err) throw err;
+    console.log("connected to database!");
+    start();
 })
+
+cfonts.say('Store Inventory Tracker', {
+    font: 'block',
+    align: 'left',
+    colors: ['green'],
+    background: 'transparent',
+    letterSpacing: 1,
+    lineHeight: 1,
+    space: true,
+    maxLength: '0',
+    gradient: false,
+    independentGradient: false,
+    transitionGradient: false,
+    env: 'node',
+})
+
+function start() {
+    inquirer
+    .prompt({
+
+    });
+}
